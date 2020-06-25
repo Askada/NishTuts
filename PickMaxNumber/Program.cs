e2380c73dd314bfe46d6b498ecff5a9f56747671
Askada <65916558+Askada@users.noreply.github.com>
2020-06-25 20:55:47 +0200

    Add files via upload

---
+++ PickMaxNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace PickMaxNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            int[] loottable = {33, 31, 42, 8, 15, 64, 59, 44, 29};
+            //int max = loottable.Max();
+            //Console.WriteLine(max);
+            int max = loottable[0];
+
+           
+
+            for (int i = 1; i < loottable.Length; i++)
+            {
+                if (max < loottable[i])
+                {
+                    max = loottable[i];
+                }
+
+            }
+
+            Console.WriteLine($"Max is {max}");
+
+        }
+
+    }
+}
---
+++ PickMaxNumber2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PickMaxNumber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] loottable = GenerateRandomTable(10);
+            int max = PickMax(loottable);
+            Console.WriteLine($"Max is {max}");
+        }
+
+
+        static int PickMax(int[] randomelements)
+        {
+            int max2 = randomelements[0];
+            for (int i = 1; i < randomelements.Length; i++)
+            {
+                if (max2 < randomelements[i])
+                {
+                    max2 = randomelements[i];
+                }
+
+            }
+            return max2;
+
+        }
+
+        static int[] GenerateRandomTable(int count)
+        {
+            int[] randomtable1 = new int[count];
+            for (int i = 0; i < randomtable1.Length; i++)
+            {
+                randomtable1[i] = new Random().Next(5000);
+                Console.WriteLine(randomtable1[i]);
+            }
+            return randomtable1;
+        }
+
+    }
+}
